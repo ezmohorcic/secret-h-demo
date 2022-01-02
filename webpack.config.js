01c85f7ffce88399cd717b405611979d9caec69c
ezmohorcic <ezmohorcic@gmail.com>
2022-01-02 16:50:28 -0300

    draft 2/1/22 16.43 first try-webpack;react install

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mode:'development',
+    entry: [
+      './front/index.js'
+    ],
+    devtool:'inline-source-map',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  }
