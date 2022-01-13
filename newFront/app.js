62ef2cb9f1ca41ad6ccaec0eaf2b2bbc19434455
ezmohorcic <ezmohorcic@gmail.com>
2022-01-12 22:33:26 -0300

    draft 12/1/22 22.33

--- newFront/app.js
+++ newFront/app.js
@@ -4,7 +4,9 @@ import Header from "./Header/Header.jsx";
 import Players from "./Players/Players.jsx"
 import Stats from "./Stats/Stats.jsx"
 import Hud from "./Hud/Hud.jsx"
+import { useDispatch, useSelector } from "react-redux";
 
+import store from "./redux/store.js"
 import {setAllPlayer_data,setPlayer_position,setPlayer_rol,unAlive,setOtherPlayer_Death,setAll_player,setNew_player,soyCeroFalse,soyCeroTrue,setStats_turno,setKnownRols} from "./redux/actions.js"
 
 const socket = io.connect('http://localhost:3000/')
@@ -12,6 +14,7 @@ export const SocketContext = React.createContext()
 
 function App() 
 {
+    const dispatch = useDispatch();
     //---
     const [all_players,setAll_players]=useState([]);
     const [player_data,setPlayer_data]=useState({});
@@ -29,60 +32,61 @@ function App()
         socket.on("your_data",function(msg)
         {
             setPlayer_data(msg);
-            //dispatch(setAllPlayer_data("msg"))
+            //store.dispatch(setAllPlayer_data("msg"))
             msg.position==0 ? setSoyCeroView({display:"block"}) :setSoyCeroView({display:"none"});
         });
 
         socket.on("new_position",function(msg)
         {
             setPlayer_data(msg.position);
-            //dispatch(setAllPlayer_data("msg.position"))
+            //store.dispatch(setAllPlayer_data("msg.position"))
             setAll_players(msg.players);
-            //dispatch(setAll_player(msg.players))
+            //store.dispatch(setAll_player(msg.players))
             msg.position.position==0 ? setSoyCeroView({display:"block"}) :setSoyCeroView({display:"none"});
-            //msg.position.position==0 ? soyCeroFalse(false) : soyCeroTrue(true);
+            //msg.position.position==0 ? store.soyCeroFalse(false) : store.soyCeroTrue(true);
         });
 
         socket.on("new_player",function(msg)
         {
             setAll_players(msg);
-            //dispatch(setNew_player(msg))
+            //store.dispatch(setNew_player(msg))
         });
 
         socket.on("change_username_on_position",function(msg)
         {
             setAll_players(msg);
-            //dispatch(setPlayer_position(msg)) requiere msg.position y msg.username
+            //store.dispatch(setPlayer_position(msg)) requiere msg.position y msg.username
         });
   
         socket.on("your_rol",function(msg)
         {
             setPlayer_data(msg);
-            //dispatch(setPlayer_rol(msg)) //msg=jugadores[n].rol
+            //store.dispatch(setPlayer_rol(msg)) //msg=jugadores[n].rol
         });
 
         socket.on("init_game_client",function(msg)
         {
             setStats_turno(msg.stats);
-            //dispatch(setStats_turno(msg.stats))
+            //store.dispatch(setStats_turno(msg.stats))
             setSoyCeroView({display:"none"});
-            //dispatch(soyCeroFalse(false))
+            //store.dispatch(soyCeroFalse(false))
             setAll_players(msg.jugadores);
-            //dispatch(setAll_player(msg.jugadores))
+            //store.dispatch(setAll_player(msg.jugadores))
         });
 
         socket.on("assasinated",function(msg)
         {
             console.log("assasinated")
             setPlayer_data(msg);
-            //dispatch(msg) necesita posicion de la muerte nada mas
+            //store.dispatch(msg) necesita posicion de la muerte nada mas
             setAlive(false);
-            //dispatch(unAlive(false)))
+            //store.dispatch(unAlive(false)))
             alert("Has sido asesinado por orden del Primer Ministro");
         });
 
         socket.on("assasination",function(msg)
         {setAll_players(msg)
+            //store.dispatch(setOtherPlayer_Death(msg))
          console.log("assasination")
         });
     },[socket]);
--- newFront/redux/reducers.js
+++ newFront/redux/reducers.js
@@ -20,24 +20,25 @@ function all_players(state=initialState,action)
                 if(copyPlayer.position==action.payload.position){copyPlayer.username=action.payload.username}
                 return copyPlayer;
             });
-        return (nwArr);
+        return ({...state,all_players:nwArr});
+    }
+    else if(action.type=="ASSASINATION")
+    {
+        let nwArr= state.map(player=>
+            {
+                let copyPlayer={...player};
+                if(copyPlayer.position==action.payload.position){copyPlayer.estado="dead"}
+                return copyPlayer;
+            });
+        return ({...state,all_players:nwArr});
     }
 }
 
 function player_data(state=initialState,action)
 {
-    if(action.type=="YOUR_DATA")
-    {
-        return action.payload
-    }
-    else if(action.type=="YOUR_ROL")
-    {
-        return {...state,player:{...state.player_data,rol:action.payload}}
-    }
-    else if(action.type=="NEW_POSITION")
-    {
-        return {...state,player:{...state.player_data,position:action.payload}}
-    }
+    if(action.type=="YOUR_DATA"){return action.payload}
+    else if(action.type=="YOUR_ROL"){return {...state,player:{...state.player_data,rol:action.payload}}}
+    else if(action.type=="NEW_POSITION"){return {...state,player:{...state.player_data,position:action.payload}}}
 }
 
 function soyCeroView(state=initialState,action)
@@ -58,16 +59,7 @@ function knownRols (state=initialState,action)
 
 function alive(state=initialState,action)
 {
-    if(action.type=="ASESINADO")
-    {
-        let nwArr= state.map(player=>
-            {
-                let copyPlayer={...player};
-                if(copyPlayer.position==action.payload.position){copyPlayer.estado="dead"}
-                return copyPlayer;
-            });
-        return (nwArr);
-    }
+    if(action.type=="ASESINADO"){return {...state,alive:false}}
 }
 
 const rootReducer=combineReducers({
